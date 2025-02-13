e5bb05cb5cf6062aa02adbdc145a43c3b1c0e026
saboooor <saboor547@gmail.com>
2025-02-13 01:20:13 -0700

    make it work properly :)

--- packages/ui/src/components/bg.ts
+++ packages/ui/src/components/bg.ts
@@ -2,33 +2,26 @@ import type { PluginAPI } from 'tailwindcss/types/config';
 
 export default function ({ matchUtilities, theme }: PluginAPI) {
   const colors: { [key: string]: string } = {};
-  Object.entries(theme('colors')!).forEach(([color, value]) => {
-    if (typeof value == 'string') {
-      colors[color] = color;
-    }
-    else {
-      Object.keys(value).forEach((shade) => {
-        colors[`${color}-${shade}`] = `${color}.${shade}`;
-        for (let i = 1; i < 20; i++) {
-          colors[`${color}-${shade}/${i*5}`] = `${color}.${shade}/${i*5}`;
-        }
-      });
+  Object.keys(theme('colors')!).forEach((color) => {
+    colors[color] = color;
+    for (let i = 1; i < 20; i++) {
+      colors[`${color}/${i*5}`] = `${color}/${i*5/100}`;
     }
   });
   matchUtilities({
     'lum-bg': (value) => {
       const color = value.split('-')[0];
+      const opacity = '/' + (value.split('/')[1] ?? 1);
       const shades = Object.values(colors).filter((c) => {
         return c.startsWith(color) && !c.includes('/');
       });
       const index = shades.indexOf(value.split('/')[0]);
       const borderColor = shades[index - 1 < 0 ? shades.length - 1 : index - 1] || shades[index + 1 > shades.length - 1 ? 0 : index + 1];
       const textColor = shades[index - 5 < 0 ? shades.length - 1 : index - 5] || shades[index + 5 > shades.length - 1 ? 0 : index + 5];
-
       return {
-        [`@apply border bg-${theme(`colors.${value.split('/')[0]}`) ? value : `[${value}]`}`]: {},
         color: textColor == value ? 'inherit' : theme(`colors.${textColor}`) ?? 'inherit',
-        borderColor: `${theme(`colors.${borderColor ?? value}`) ?? borderColor ?? value}`,
+        backgroundColor: `${theme(`colors.${value}`) ?? borderColor ?? value}`,
+        border: `1px solid ${theme(`colors.${borderColor ?? value}${opacity}`) ?? borderColor ?? value}`,
         outline: 'none',
         '&:focus': {
           border: `1px solid ${textColor == value ? 'white' : theme(`colors.${textColor}`) ?? 'white'}`,
