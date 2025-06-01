8da407294cc68d991735b179c9159edb366ca3d6
saboooor <saboor547@gmail.com>
2025-06-01 16:53:11 -0600

    fix logo size

--- packages/ui-docs/src/components/Nav.tsx
+++ packages/ui-docs/src/components/Nav.tsx
@@ -11,7 +11,7 @@ export default component$(() => {
           class="flex items-center gap-1 fill-[#f0ccfb] font-semibold text-[#f0ccfb]"
           style="filter: drop-shadow(0 0 1rem #CB6CE6);"
         >
-          <LogoLuminescentFull size={100} class="mt-1" />
+          <LogoLuminescentFull size={20} class="mt-1" />
         </div>
       </Link>
 
@@ -46,7 +46,7 @@ export default component$(() => {
         class="lum-btn lum-bg-transparent"
       >
         <div class="flex items-center gap-1 font-semibold">
-          <LogoLuminescentFull size={100} class="mt-1" />
+          <LogoLuminescentFull size={20} class="mt-1" />
         </div>
       </a>
       <div q:slot="mobile" class="flex justify-evenly">
