762d2acd5535757e21ae31041c8e6f8e9b93bb89
saboooor <saboor547@gmail.com>
2026-01-19 01:48:53 -0500

    fix: adjust RangeInput layout to use 'justify-evenly' and add min support

--- packages/ui-qwik/src/components/elements/RangeInput.tsx
+++ packages/ui-qwik/src/components/elements/RangeInput.tsx
@@ -31,24 +31,20 @@ export const RangeInput = component$<RangeInputProps>((props) => {
 });
 
 export const RangeInputRaw = component$<RangeInputRawProps>(
-  ({ value = 0, max = 100, onInput$, ...props }) => {
-    const valueSignal = useSignal<number>(value);
-    const filledPercentage = useComputed$(() => (valueSignal.value / max) * 100);
+  ({ value, min = 5, max = 100, onInput$, ...props }) => {
+    const valueSignal = useSignal<number>(value ?? min);
+    const filledPercentage = useComputed$(() => ((valueSignal.value - min) / (max - min)) * 100);
 
     return (
-      <div
-        class={{
-          'group relative flex touch-manipulation gap-1 text-lum-text lum-input p-0': true,
-        }}
-      >
-        <div class="absolute w-full flex justify-between">
-          {[...Array(max)].map((_, i) => {
-            if (i === 0 || i === max - 1) return null;
+      <div class={{
+        'group relative flex touch-manipulation gap-1 text-lum-text lum-input p-0': true,
+      }}>
+        <div class="absolute w-full flex justify-evenly">
+          {[...Array(max - min + 1)].map((_, i) => {
+            if (i === 0 || i === max - min) return null;
 
-            return <div
-              key={i}
+            return <div key={i}
               class="border-l border-l-lum-border/20 h-1 my-0.5"
-              style={{ left: `${(i / 4) * 100}%` }}
             />;
           })}
         </div>
@@ -63,6 +59,7 @@ export const RangeInputRaw = component$<RangeInputRawProps>(
         </div>
         <input
           {...props}
+          min={min}
           max={max}
           type="range"
           value={valueSignal.value}
