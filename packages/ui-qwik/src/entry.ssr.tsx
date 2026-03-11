359265a7c0d3c875fd2875c1791750bfede7a16c
saboooor <saboor547@gmail.com>
2026-03-11 07:54:53 -0400

    fix: add type import to suppress TypeScript warnings

--- packages/ui-qwik/src/entry.ssr.tsx
+++ packages/ui-qwik/src/entry.ssr.tsx
@@ -7,6 +7,10 @@
  * On the client, containers resume and do not call render.
  */
 import { createRenderer } from "@qwik.dev/router";
+
+// make typescript shut up
+import type { } from "@qwik.dev/core/server";
+
 import Root from "./root";
 
 export default createRenderer((opts) => {
