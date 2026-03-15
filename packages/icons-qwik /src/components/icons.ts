6a34bcbcc11b333d914004fe77e0bd7577500b2a
saboooor <saboor547@gmail.com>
2026-03-15 20:20:21 -0300

    move icons to components

--- packages/icons-qwik /src/components/icons.ts
+++
@@ -1,7 +0,0 @@
-export * from './icons/Birdflop';
-export * from './icons/Fabric';
-export * from './icons/Forge';
-export * from './icons/Luminescent';
-export * from './icons/Paper';
-export * from './icons/Purpur';
-export * from './icons/Waterfall';
