6c77eec9713ecc3e360cc8d3f71fd9f548bbc5af
saboooor <github@saboor.ca>
2026-07-13 18:36:17 -0400

    refactor: enhance Label component styling and class handling

--- packages/ui-qwik/src/components/elements/Label.tsx
+++ packages/ui-qwik/src/components/elements/Label.tsx
@@ -1,5 +1,6 @@
 import type { PropsOf } from '@qwik.dev/core';
 import { component$, Slot } from '@qwik.dev/core';
+import { getClassObject } from '../functions';
 
 interface LabelProps extends PropsOf<'label'> {
   label?: string,
@@ -12,8 +13,14 @@ export const Label = component$<LabelProps>(({
   ...props
 }) => {
   return (
-    <div class="flex flex-col" {...outerProps}>
-      <label {...props} class="pb-1 text-lum-text select-none">
+    <div class={{
+      "flex flex-col gap-1": true,
+      ...getClassObject(outerProps?.class),
+    }} {...outerProps}>
+      <label {...props} class={{
+        "pb-1 text-lum-text select-none": true,
+        ...getClassObject(props.class),
+      }}>
         {label}
         <Slot q:slot="label" />
       </label>
